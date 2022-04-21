c789e1e02249f8663eb02543f5d8deeaecb19203
Phan Tran Hao <haopt195@gmail.com>
2022-04-21 22:01:52 +0700

    Testing Login

---
+++ selenium/javaTester/Topic_03_Random.java
@@ -0,0 +1,16 @@
+package javaTester;
+
+import java.util.Random;
+
+public class Topic_03_Random {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		System.out.println(rand.nextInt(9999));
+		System.out.println(rand.nextInt(9999));
+		System.out.println(rand.nextInt(9999));
+		System.out.println(rand.nextInt(9999));
+
+	}
+
+}
