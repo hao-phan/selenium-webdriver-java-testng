49872571ca9994ceae25c71fa193fbd036667505
Phan Tran Hao <haopt195@gmail.com>
2022-06-19 15:56:00 +0700

    Update Custom dropdown

--- selenium/webdriver/Topic_10_Handle_Custom_Dropdown.java
+++ selenium/webdriver/Topic_10_Handle_Custom_Dropdown.java
@@ -34,49 +34,125 @@ public class Topic_10_Handle_Custom_Dropdown {
 		driver.manage().window().maximize();
 	}
 
+	/* Từ TC_01 -> TC_04 là Custom_Dropdown */
 	@Test
 	public void TC_01_JQuery() {
 		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
 		
 		// Chọn item 10 
-		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "10"); //Viết 1 cái hàm bên dưới để dễ quản lí và dùng lại được nhiều lần
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "10"); //Viết 1 cái hàm Wrapper bên dưới để dễ quản lí và dùng lại được nhiều lần
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']")).getText(), "10");
 
 		// Chọn item 5
-		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "5"); //Viết 1 cái hàm bên dưới để dễ quản lí và dùng lại được nhiều lần
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "5");
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']")).getText(), "5");
+	}
+	
+	@Test
+	public void TC_02_ReactJS() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+			
+		// Chọn "Matt"
+		selectItemInCustomDropdown("//div[@class='ui fluid selection dropdown']", "//div[@class='ui active visible fluid selection dropdown']//span", "Matt");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Matt");
+		
+		// Chọn "Stevie Feliciano"
+		selectItemInCustomDropdown("//div[@class='ui fluid selection dropdown']", "//div[@class='ui active visible fluid selection dropdown']//span", "Stevie Feliciano");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Stevie Feliciano");
+	}
+	
+	@Test
+	public void TC_03_VueJS() {
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+			
+		// Chọn "Second Option"
+		selectItemInCustomDropdown("//li[@class='dropdown-toggle']", "//li[@class='dropdown-toggle']/following-sibling::ul/li/a", "Second Option");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='btn-group']")).getText(), "Second Option");
+
+		// Chọn "Third Option"
+		selectItemInCustomDropdown("//li[@class='dropdown-toggle']", "//li[@class='dropdown-toggle']/following-sibling::ul/li/a", "Third Option");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='btn-group']")).getText(), "Third Option");
+	}
+	
+	@Test
+	public void TC_04_Editable_Dropdown() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+			
+		enterItemInCustomDropdown("//input[@class='search']", "//div[@class='visible menu transition']/div/span", "Andorra");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Andorra");
+
+		enterItemInCustomDropdown("//input[@class='search']", "//div[@class='visible menu transition']/div/span", "Bahrain");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Bahrain");
 		
 	}
 	
+	/*TC_05 là Default_Dropdown nhưng cũng có thể sử dụng hàm Wrapper của Custom_Dropdown được */
+	@Test
+	public void TC_05_DefaultDropdown_Using_WrapperCustomDropdown() {
+		driver.get("https://demo.nopcommerce.com/register");
+
+		selectItemInCustomDropdown("//select[@name='DateOfBirthDay']", "//select[@name='DateOfBirthDay']/option", "10");
+		Assert.assertTrue(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']/option[text()='10']")).isSelected()); //Dùng isSelected
+
+
+		selectItemInCustomDropdown("//select[@name='DateOfBirthDay']", "//select[@name='DateOfBirthDay']/option", "5");
+		Assert.assertTrue(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']/option[text()='5']")).isSelected());				
+	}
 	
-	//Viết hàm để có thể sử dụng lại được nhiều lần
+	
+	
+	//Viết hàm để có thể sử dụng lại được nhiều lần -> Hay còn được gọi là Wrapper
 	public void selectItemInCustomDropdown(String parentLocator, String childLocator, String expectedLocator) {
 		// Click vào dropdown cho xổ hết ra tất cả các item con bên trong => Click
-				driver.findElement(By.xpath(parentLocator)).click(); 
+		driver.findElement(By.xpath(parentLocator)).click();
+		sleepInSecond(1);
+
+		// Chờ cho tất cả các item con bên trong được load ra => WebDriverWait
+		// By Locator = đại diện cho "Tất cả các item con bên trong"
+		// Lấy cái locator đến cái thẻ chưa text item
+		expliciWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childLocator)));
+
+		// Tìm item mong muốn (nếu như ko hiển thị thì cần cuộn chuột xuống để tìm
+		// =>Vòng lặp để lấy hết tất cả các item rồi duyệt qua - getText từng cái
+
+		// Lấy hết tất cả các item ra lưu vào 1 List Element
+		List<WebElement> allDropDowItems = driver.findElements(By.xpath(childLocator)); // 19 items trong này
+
+		// Duyệt qua vòng lặp
+		for (WebElement item : allDropDowItems) {
+			String actualTextItem = item.getText();
+			System.out.println("Item text = " + actualTextItem);
+
+			/// Thấy item cần chọn thì click vào => So sánh với item mong muốn sau đó Click
+			/// vào
+			if (actualTextItem.equals(expectedLocator)) {
+				item.click();
 				sleepInSecond(1);
-				
-				// Chờ cho tất cả các item con bên trong được load ra => WebDriverWait
-				// By Locator = đại diện cho "Tất cả các item con bên trong"
-				// Lấy cái locator đến cái thẻ chưa text item
-				expliciWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childLocator)));
-				
-				//Tìm item mong muốn (nếu như ko hiển thị thì cần cuộn chuột xuống để tìm
-				// =>Vòng lặp để lấy hết tất cả các item rồi duyệt qua - getText từng cái
-				
-				//Lấy hết tất cả các item ra lưu vào 1 List Element
-				List<WebElement> allDropDowItems = driver.findElements(By.xpath(childLocator)); //19 items trong này
-				
-				//Duyệt qua vòng lặp
-				for (WebElement item : allDropDowItems) {
-					String actualTextItem = item.getText();
-					System.out.println("Item text = " + actualTextItem);
-					
-					/// Thấy item cần chọn thì click vào => So sánh với item mong muốn sau đó Click vào
-					if(actualTextItem.equals(expectedLocator)) {
-						item.click();
-						sleepInSecond(1);
-						break;//Thoát khỏi vòng lặp khi đã chọn được giá trị mong muốn
-					}
-				}
+				break;// Thoát khỏi vòng lặp khi đã chọn được giá trị mong muốn
+			}
+		}
 	}
+	
+	public void enterItemInCustomDropdown(String editableLocator, String childLocator, String expectedLocator) {
+		
+		driver.findElement(By.xpath(editableLocator)).sendKeys(expectedLocator);
+		sleepInSecond(1);
+
+		expliciWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childLocator)));
+		List<WebElement> allDropDowItems = driver.findElements(By.xpath(childLocator)); // 19 items trong này
+
+		for (WebElement item : allDropDowItems) {
+			String actualTextItem = item.getText();
+			System.out.println("Item text = " + actualTextItem);
+			if (actualTextItem.equals(expectedLocator)) {
+				item.click();
+				sleepInSecond(1);
+				break;
+			}
+		}
+	}
+	
+	
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
